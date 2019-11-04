4daf23592e01581ac2d7d354d53b589ee58c1569
listv <akuziakov@gmail.com>
2019-11-04 23:17:52 +0200

    191104 getting started from work (#5)
    * Passing and Using Data with Event Binding
    
    * Two-way data binding.
    
    * Combining all forms of DataBinding.
    
    * Using ngIf to Output Data
    Conditionally
    
    * Enhancing ngIf with an Else Condition.
    
    * Styling Elements Dynamically with
    ngStyle.
    
    * Applying CSS Classes Dynamically with
    ngClass.
    
    * Outputting lists with ngFor.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `
+  ]
 })
 export class ServerComponent {
+  public serverId = 10;
+  public serverStatus = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  public getServerStatus() {
+    return this.serverStatus;
+  }
+
+  public getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  public allowNewServer = false;
+  public serverCreationStatus = 'No server was created';
+  public serverName = 'TestServer';
+  public isServerCreated = false;
+  public servers = ['TestServer', 'TestServer2'];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  onCreateServer() {
+    this.isServerCreated = true;
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
+  }
+
+  onUpdateServerName(event) {
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
+  ngOnInit() {}
 }
