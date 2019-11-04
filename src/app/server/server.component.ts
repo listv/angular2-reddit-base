bd1bbeff7fa18d3536c54ca97c7fc8e81dd981f2
Oleksandr Kuziakov <Oleksandr_Kuziakov@epam.com>
2019-11-04 16:54:20 +0200

    Styling Elements Dynamically with ngStyle.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,7 +8,15 @@ export class ServerComponent {
   public serverId = 10;
   public serverStatus = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   public getServerStatus() {
     return this.serverStatus;
+  }
+
+  public getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
